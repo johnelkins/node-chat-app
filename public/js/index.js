61e7142fcaaaf319b3fb9897c2c938150e9aff37
theinternot <theinternot@gmail.com>
2017-06-07 11:06:57 -0700

    Add geolocation test case

--- public/js/index.js
+++ public/js/index.js
@@ -24,6 +24,17 @@
      console.log('Message from the Admin', message);
  });
 
+ socket.on('newLocationMessage', function(message) {
+     var li = jQuery('<li></li>');
+     var a = jQuery('<a target="_blank">My current location</a>')
+
+     li.text(`${message.from}: `);
+     a.attr('href', message.url);
+
+     li.append(a);
+     jQuery('#messages').append(li);
+ });
+
  //  socket.emit('createMessage', {
  //      from: 'Frank',
  //      text: 'Hi'
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -18,4 +18,24 @@ describe('generateMessage', () => {
         // assert text match
         // assert createdAt is number
     })
+});
+
+describe('generate location message', () => {
+    it('should generate correct location object', () => {
+        // store res in variable 
+        var from = 'Admin';
+        var latitude = 15;
+        var longitude = 19;
+        var url = 'https://www.google.com/maps?q=15,19'
+        var message = generateLocationMessage(from, latitude, longitude);
+        // console.log(message);
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({
+            from,
+            url
+        });
+        // assert from match
+        // assert text match
+        // assert createdAt is number
+    });
 });

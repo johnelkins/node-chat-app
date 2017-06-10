03f5ef97920d0007483d9d89524d44204a187cd7
theinternot <theinternot@gmail.com>
2017-06-10 15:08:51 -0700

    Add mustache js template

--- public/js/index.js
+++ public/js/index.js
@@ -9,12 +9,20 @@
  });
 
  socket.on('newMessage', function(message) {
-     console.log('New message', message);
      var formattedTime = moment(message.createdAt).format('h:mm a');
-     var li = jQuery('<li></li>');
-     li.text(`${message.from} ${formattedTime}: ${message.text}`);
+     var template = jQuery('#message-template').html();
+     var html = Mustache.render(template, {
+         text: message.text,
+         from: message.from,
+         createdAt: formattedTime
+     });
 
-     jQuery('#messages').append(li);
+     jQuery('#messages').append(html);
+
+     //  console.log('New message', message);
+     //  var li = jQuery('<li></li>');
+     //  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+     //  jQuery('#messages').append(li);
  });
 
  socket.on('welcomeMessage', function(message) {
@@ -26,15 +34,26 @@
  });
 
  socket.on('newLocationMessage', function(message) {
+     console.log('ifired');
+
      var formattedTime = moment(message.createdAt).format('h:mm a');
-     var li = jQuery('<li></li>');
-     var a = jQuery('<a target="_blank">My current location</a>')
+     var template = jQuery('#location-message-template').html();
+     var html = Mustache.render(template, {
+         from: message.from,
+         url: message.url,
+         createdAt: formattedTime
+     });
 
-     li.text(`${message.from} ${formattedTime}: `);
-     a.attr('href', message.url);
+     jQuery('#messages').append(html);
+     //  var formattedTime = moment(message.createdAt).format('h:mm a');
+     //  var li = jQuery('<li></li>');
+     //  var a = jQuery('<a target="_blank">My current location</a>')
 
-     li.append(a);
-     jQuery('#messages').append(li);
+     //  li.text(`${message.from} ${formattedTime}: `);
+     //  a.attr('href', message.url);
+
+     //  li.append(a);
+     //  jQuery('#messages').append(li);
  });
 
  //  socket.emit('createMessage', {
